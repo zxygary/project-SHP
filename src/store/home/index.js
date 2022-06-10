9b02c5f2f7361755988cc4f1f0379bf7e927abb3
zxygary <zxy@zxygary.com>
2022-06-10 13:59:29 +0800

    一级分类动态添加类名(通过事件委托实现)

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,7 +1,23 @@
+import {reqCategoryList} from '@/api'
 // home模块的小仓库
-const state = {};
-const mutations = {};
-const actions = {};
+const state = {
+  // state中数据默认初始值是根据接口的返回值来进行初始化的
+  categoryList:[],
+};
+const mutations = {
+  CATEGORYLIST(state, categoryList) {
+    state.categoryList = categoryList;
+  }
+};
+const actions = {
+  // 通过API里面的接口函数调用，向服务器发请求，获取服务器数据
+  async categoryList(commit) {
+    let result = await reqCategoryList();
+      if(result.code==200) {
+        commit("CATEGORYLIST", result.data);
+      }
+  }
+};
 const getters = {};
 export default {
   state,
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,15 @@
-import Vue from "vue";
+import Vue from 'vue';
 import Vuex from 'vuex';
 // 需要使用插件一次
 Vue.use(Vuex);
 // 引入小仓库
-import home from "./home";
-import search from "./search";
+import home from './home';
+import search from './search';
 // 对外暴露Store类的一个实例
 export default new Vuex.Store({
   namespaced: true,
   // 实现Vuex仓库模块式开发存储数据
-  modules:{
+  modules: {
     home,
     search
   }
